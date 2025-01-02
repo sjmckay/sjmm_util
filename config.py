78ce32a1d51030566e09e06b76f9f5c2c6c8eea9
stephen@berea <sjmckay3@wisc.edu>
2025-01-02 09:58:03 -0600

    add config func

---
+++ config.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+from matplotlib import font_manager
+
+
+def set_plot_defaults()
+	plt.rcParams['text.usetex'] = False
+	plt.rcParams['font.size'] = '14'
+	plt.rcParams["font.family"] = "serif"
+	plt.rcParams["font.serif"] = "Times New Roman"
+	plt.rcParams['mathtext.fontset'] = 'cm'
+
+	for fontpath in font_manager.findSystemFonts(fontpaths=None, fontext='ttf'):
+	    if 'Times'.lower() in fontpath.lower():
+	        font_manager.fontManager.addfont(fontpath)

9dfc578033ce6293ec29a1fad738c3b7ca0796a3
stephen@berea <sjmckay3@wisc.edu>
2025-08-06 11:39:52 -0500

    Fixed style sheet import and resource management

--- sjmm_util/config.py
+++ sjmm_util/config.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 from matplotlib import font_manager
-
+import importlib.resources as resources
+import sjmm_util
 
 def set_plot_defaults():
-	plt.style.use('sjmm_util.sjmm_util.sjmdefaults')
+	with resources.path(sjmm_util, "sjmdefaults.mplstyle") as style_path:
+		plt.style.use(str(style_path))
 
 	for fontpath in font_manager.findSystemFonts(fontpaths=None, fontext='ttf'):
 	    if 'Times'.lower() in fontpath.lower():

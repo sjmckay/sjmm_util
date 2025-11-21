18f4e846f4b330fcfa82b1bd2a73f53ee04097fe
stephen@berea <sjmckay3@wisc.edu>
2025-11-21 14:13:41 -0600

    add versioning

--- sjmm_util/__init__.py
+++ sjmm_util/__init__.py
@@ -1,1 +1,8 @@
 from . import config, catalog, general_utils, datatools, line_utils, scuba2_utils
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("sjmm_util")
+except PackageNotFoundError:
+    __version__ = "0.0.0"

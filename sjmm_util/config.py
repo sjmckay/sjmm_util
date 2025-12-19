9d2ac53d27d8c661e5258f64f34a0f55c60bf4bd
stephen@berea <sjmckay3@wisc.edu>
2025-12-19 12:16:13 -0600

    multiple plot styles

--- sjmm_util/config.py
+++ sjmm_util/config.py
@@ -4,9 +4,26 @@ import importlib.resources as resources
 import sjmm_util
 
 def set_plot_defaults():
-	with resources.path(sjmm_util, "sjmdefaults.mplstyle") as style_path:
+	plt.style.use('default')
+	with resources.path(sjmm_util, "mydefaults.mplstyle") as style_path:
 		plt.style.use(str(style_path))
+	add_font('Times')
 
+
+def set_plot_defaults_paper():
+	plt.style.use('default')
+	with resources.path(sjmm_util, "paper.mplstyle") as style_path:
+		plt.style.use(str(style_path))
+	add_font('Times')
+
+def set_plot_defaults_talk():
+	plt.style.use('ggplot')
+	plt.style.use('dark_background')
+	with resources.path(sjmm_util, "talk.mplstyle") as style_path:
+		plt.style.use(str(style_path))
+	add_font('Times')
+	
+def add_font(font):
 	for fontpath in font_manager.findSystemFonts(fontpaths=None, fontext='ttf'):
-	    if 'Times'.lower() in fontpath.lower():
+	    if font.lower() in fontpath.lower():
 	        font_manager.fontManager.addfont(fontpath)
